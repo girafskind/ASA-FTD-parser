7c5c492fc2ce062b29a752420151bbf69b12e908
girafskind <bmo@girafskind.dk>
2021-12-20 00:06:52 +0100

    deploy function

---
+++ fdmdeploy.py
@@ -0,0 +1,19 @@
+# ASA-FTD object parser
+# fdmdeploy.py - Deploy FDM config
+# Version 0.1
+# By Bo V Mortensen
+
+import requests
+
+def deployfdm(fdm):
+    url = "https://" + fdm.ip + ":" + fdm.port + "/api/fdm/v6/operational/deploy"
+
+    payload = {}
+
+    headers = {
+        'Accept': 'application/json',
+        'Authorization': 'Bearer ' + fdm.access_token
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload, verify=False).json()
+    print(response)
--- main.py
+++ main.py
@@ -8,38 +8,14 @@ import config
 import classes
 import net_objects
 import netgroup_objects
+import fdmdeploy
 
 def main():
     #testfdm()
-    testasa()
-    #hawkasa01 = classes.ASAClass()
-    #hawkasa01.ip = config.asaip
-    #hawkasa01.username = config.asausername
-    #hawkasa01.password = config.asapassword
-    #hawkasa01.port = config.asaport
-    #hawkasa01.token = authorize.asagettoken(hawkasa01)
+    #testasa()
+    testfdmdeploy()
 
-    #asaobj = net_objects.getallasanetworkobjects(hawkasa01)
-    #for object in asaobj:
-    #    print(object)
-#    asaobj = netgroup_objects.getallasanetworkgroups(hawkasa01)
-#    for object in asaobj:
-#        print("Group name")
-#        print(object['name'])
-#        print("Group members")
-#        for member in object['members']:
-#            print(member.get('objectId'))
 
-#    objects.getasanetworkobjects(hawkasa01)
-#    hawkfp01 = classes.FTDClass()
-#    hawkfp01.ip = config.ftdip
-#    hawkfp01.username = config.ftdusername
-#    hawkfp01.password = config.ftdpassword
-
-#    authorize.fdmgettoken(hawkfp01)
-
-#    print(hawkfp01.expires)
-#    print(hawkfp01.refresh_expires)
 def testasa():
     hawkasa01 = classes.ASAClass()
     hawkasa01.ip = config.asaip
@@ -69,6 +45,15 @@ def testfdm(object):
     print(hawkfp01.expires)
     print(hawkfp01.refresh_expires)
     net_objects.createfdmnetworkobject(hawkfp01,object)
+
+def testfdmdeploy():
+    hawkfp01 = classes.FTDClass()
+    hawkfp01.ip = config.ftdip
+    hawkfp01.username = config.ftdusername
+    hawkfp01.password = config.ftdpassword
+    authorize.fdmgettoken(hawkfp01)
+
+    fdmdeploy.deployfdm(hawkfp01)
 
 if __name__ == '__main__':
     main()
