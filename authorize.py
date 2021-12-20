2e9784c3dee96ae991f536998d900bf3b5c9318b
girafskind <bmo@girafskind.dk>
2021-12-20 13:40:29 +0100

    removed FxOS token and logout functions

--- authorize.py
+++ authorize.py
@@ -27,38 +27,6 @@ def fdmgettoken(fdm):
     fdm.refresh_expires = now + datetime.timedelta(seconds=response['refresh_expires_in'])
     return response
 
-def fxosgettoken(ftd):
-    url = "https://" + ftd.ip + ":" + ftd.port + "/api/login"
-
-    auth = HTTPBasicAuth(ftd.username, ftd.password)
-
-    payload = {}
-    headers = {
-      'Content-Type': 'application/json',
-      'User-agent': 'REST API Agent',
-      'username': ftd.username,
-      'password': ftd.password
-    }
-
-    response = requests.request("POST", url, headers=headers, data=payload, verify=False).text
-    config.FXOSTOKEN = json.loads(response)
-
-    return response
-
-def fxoslogout(ftd):
-    url = "https://" + ftd.ip + ":" + ftd.port + "/api/login"
-
-    payload = {}
-
-    headers = {
-        'Authorization': 'Bearer '+str(ftd.token['token']),
-        'accept': 'application/json',
-        'Content-Type': 'application/json'
-    }
-
-    response = requests.request("POST", url, headers=headers, data=payload, verify=False).text
-
-    return response
 
 def asagettoken(asa):
     url = "https://" + asa.ip + ":" + asa.port + "/api/tokenservices"
@@ -75,17 +43,3 @@ def asagettoken(asa):
 
     return response.headers['X-Auth-Token']
 
-def asalogout(asa):
-    url = "https://" + asa.ip + ":" + asa.port + "/api/tokenservices"
-
-    basicauth = HTTPBasicAuth(asa.username, asa.password)
-
-    payload = {}
-    headers = {
-        'Content-Type': 'application/json',
-        'User-agent': 'REST API Agent'
-    }
-
-    response = requests.request("POST", url, auth=basicauth, headers=headers, data=payload, verify=False)
-
-    return response.headers['X-Auth-Token']
